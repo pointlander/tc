593c23bbfbdb2d2c6b89ead11c24b348912b210e
Andrew Snodgrass <pointlander@gmail.com>
2025-02-28 15:35:46 -0600

    Improvements

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -19,7 +19,7 @@ func main() {
 	select {}
 }
 
-func Draw(tree string) string {
+func Draw(id, tree string) string {
 	_, t := Parse([]byte(tree))
 	n := t.Label()
 	polygon := t.Triangulation(n)
@@ -27,24 +27,36 @@ func Draw(tree string) string {
 	offset := -math.Pi/2 + segment/2
 
 	document := js.Global().Get("document")
-	canvas := document.Call("getElementById", "myCanvas")
+	canvas := document.Call("getElementById", id)
+	width := float64(canvas.Get("width").Int())
+	height := float64(canvas.Get("height").Int())
+	radius := width / 2
+	if height/2 < radius {
+		radius = height / 2
+	}
 	context := canvas.Call("getContext", "2d")
 
 	context.Call("beginPath")
-	context.Call("arc", 256, 256, 248, 0, 2*math.Pi, true)
+	context.Call("arc", width/2, height/2, radius-8, 0, 2*math.Pi, true)
 	context.Call("stroke")
 
 	for i := range polygon {
 		for _, j := range polygon[i] {
 			context.Call("beginPath")
-			context.Call("moveTo", -248*math.Cos(offset+float64(i)*segment)+256, 248*math.Sin(offset+float64(i)*segment)+256)
-			context.Call("lineTo", -248*math.Cos(offset+float64(j)*segment)+256, 248*math.Sin(offset+float64(j)*segment)+256)
+			context.Call("moveTo",
+				-(radius-8)*math.Cos(offset+float64(i)*segment)+width/2,
+				(radius-8)*math.Sin(offset+float64(i)*segment)+height/2)
+			context.Call("lineTo",
+				-(radius-8)*math.Cos(offset+float64(j)*segment)+width/2,
+				(radius-8)*math.Sin(offset+float64(j)*segment)+height/2)
 			context.Call("stroke")
 		}
 	}
 	for i := range polygon {
 		context.Call("beginPath")
-		context.Call("arc", -248*math.Cos(offset+float64(i)*segment)+256, 248*math.Sin(offset+float64(i)*segment)+256,
+		context.Call("arc",
+			-(radius-8)*math.Cos(offset+float64(i)*segment)+width/2,
+			(radius-8)*math.Sin(offset+float64(i)*segment)+height/2,
 			8, 0, 2*math.Pi, true)
 		context.Set("fillStyle", "blue")
 		context.Call("fill")
@@ -54,10 +66,10 @@ func Draw(tree string) string {
 
 func drawWrapper() js.Func {
 	drawFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) != 1 {
+		if len(args) != 2 {
 			return "Invalid no of arguments passed"
 		}
-		return Draw(args[0].String())
+		return Draw(args[0].String(), args[1].String())
 	})
 	return drawFunc
 }
